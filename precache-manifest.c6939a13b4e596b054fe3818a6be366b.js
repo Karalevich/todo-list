ba059001fddbc5301ec5e4b2cfe5a24aa917c0d6
AndreyKorolevich <korolevich1994mail.ru>
2020-07-10 19:54:23 +0300

    Updates

---
+++ precache-manifest.c6939a13b4e596b054fe3818a6be366b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e480258a317a6d753b519509e8079569",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "16bb72de8d56c2e6ae4f",
+    "url": "/todo-list/static/css/main.168a2a3a.chunk.css"
+  },
+  {
+    "revision": "1d2b3b41baff96f6c2bb",
+    "url": "/todo-list/static/js/2.6037303e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo-list/static/js/2.6037303e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16bb72de8d56c2e6ae4f",
+    "url": "/todo-list/static/js/main.48070730.chunk.js"
+  },
+  {
+    "revision": "e9fc08bbe231055ac159",
+    "url": "/todo-list/static/js/runtime-main.86455fb3.js"
+  }
+]);
